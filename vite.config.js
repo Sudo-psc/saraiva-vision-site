aa1b07f460a0e996b44bd29117cb252e2b2a5362
Philipe Saraiva Cruz <philipecruz@Philipes-MacBook-Pro.local>
2025-09-20 22:13:38 -0300

    fix: temporarily disable workbox plugin for vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { workboxVitePlugin } from './src/utils/workbox-vite-plugin.js'
 
 export default defineConfig({
-  plugins: [react(), workboxVitePlugin()],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
